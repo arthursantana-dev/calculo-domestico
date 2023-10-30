67199489bfb446895c5773963417fa8b0d49d0a1
Arthur Santana <90651161+arthursantana-dev@users.noreply.github.com>
2023-10-30 10:40:32 -0300

    balanço

--- src/components/Balanco.js
+++ src/components/Balanco.js
@@ -1,14 +1,62 @@
+import { useState } from "react"
+
 export default function Balanco(props) {
+	let custo = 0
 	return(
 		<div className="wrapper-border">
 			<h1>Balanço</h1>
 			<hr />
 			<div className="custos-wrapper">
-				{/* {
-					!props.listaCustoFixo? "" : props.listaCustoFixo.map((c, i) => {
-						return()
+				<h2>
+					Custos Fixos
+				</h2>
+				{
+					props.listaCustoFixo.map((c, i) => {
+						if(c.ativo){
+							custo += c.valor
+
+							return (
+								<section key={i} className="balanco__custo-container">
+									<p>
+										{c.nome}
+									</p>
+									<p>
+										{c.valor}
+									</p>
+								</section>
+							)
+						}
+
 					})
-				} */}
+					
+				}
+				<h2>
+					Custo Misto
+				</h2>
+				{
+					props.listaCustoMisto.map((c, i) => {
+						if(c.ativo){
+							custo += c.valor
+
+							return (
+								<section key={i} className="balanco__custo-container">
+									<p>
+										{c.nome}
+									</p>
+									<p>
+										{c.valor}
+									</p>
+								</section>
+							)
+						}
+
+					})
+					
+				}
+				{
+					custo.toFixed(2)
+				}
+				
 			</div>
 		</div>
 	)
